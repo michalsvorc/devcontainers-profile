b1f219d12c3a3643f93ea877b96980ec520adcd5
Michal Svorc <dev@michalsvorc.com>
2022-05-27 12:26:54 +0200

    chore(shell): index empty lines update

--- shell/index.sh
+++ shell/index.sh
@@ -1,11 +1,10 @@
-
 #===============================================================================
 # Shared shell configuration sourced by interactive and login shells
 #===============================================================================
 
 shell_partials_dir="${HOME}/.local/profile/shell"
-
 source "${shell_partials_dir}/term.sh"
+
 source "${shell_partials_dir}/aliases.sh"
 source "${shell_partials_dir}/history.sh"
 source "${shell_partials_dir}/history_search.sh"
